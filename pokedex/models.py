61f4917700c5a8043a7e1ddc12f4f8d1feb8736a
Sebastian <sebastian.rubio@uisek.edu.ec>
2026-05-05 09:49:17 -0500

    Taller n°8

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    type = models.CharField(max_length=50, null=False)
+    height = models.FloatField(null=False)
+    weight = models.FloatField(null=False)
+    
+    def __str__(self):
+        return self.name
