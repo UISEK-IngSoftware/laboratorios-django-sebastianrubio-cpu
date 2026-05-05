9007c370e1c08778dd1f88b81a3ad080b29a1f53
Sebastian <sebastian.rubio@uisek.edu.ec>
2026-05-05 11:36:49 -0500

    Laboratorio 4 - Finalizado

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,9 +1,20 @@
+from random import choices
+
 from django.db import models
 
 # Create your models here.
 class Pokemon(models.Model):
     name = models.CharField(max_length=100, null=False)
-    type = models.CharField(max_length=50, null=False)
+    pokemon_type = {'Water','W'
+            'Fire', 'F'
+            'Grass', 'G'
+            'Electric', 'E'
+            'Psychic', 'P'
+            'Ice', 'I'
+            'Dragon', 'D'
+            'Dark', 'DK'
+            'Fairy', 'FA'}
+    type = models.CharField(max_length=30,choices=pokemon_type, null=False)
     height = models.FloatField(null=False)
     weight = models.FloatField(null=False)
     
