f1a49cadd7fdd7373c2b8f05a12816c0ee98dbe9
Sebastian <sebastian.rubio@uisek.edu.ec>
2026-05-10 19:16:10 -0400

    Creacion de Models trainer y cambio de view

--- pokedex/models.py
+++ pokedex/models.py
@@ -2,22 +2,34 @@ from random import choices
 
 from django.db import models
 
-# Create your models here.
+class Trainer(models.Model):
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    birth_date = models.DateField(null=False)
+    level = models.IntegerField(null=False)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Pokemon(models.Model):
     name = models.CharField(max_length=100, null=False)
-    pokemon_type = {'Water','W'
-            'Fire', 'F'
-            'Grass', 'G'
-            'Electric', 'E'
-            'Psychic', 'P'
-            'Ice', 'I'
-            'Dragon', 'D'
-            'Dark', 'DK'
-            'Fairy', 'FA'}
-    type = models.CharField(max_length=30,choices=pokemon_type, null=False)
+    
+    POKEMON_TYPES = [
+        ('W', 'Water'),
+        ('F', 'Fire'),
+        ('G', 'Grass'),
+        ('E', 'Electric'),
+        ('P', 'Psychic'),
+        ('I', 'Ice'),
+        ('D', 'Dragon'),
+        ('DK', 'Dark'),
+        ('FA', 'Fairy')
+    ]
+    
+    type = models.CharField(max_length=30, choices=POKEMON_TYPES, null=False)
     height = models.FloatField(null=False)
     weight = models.FloatField(null=False)
-    
     
     def __str__(self):
         return self.name
