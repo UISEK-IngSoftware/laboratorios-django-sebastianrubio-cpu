389d6c452bbc22f7d970ff426ed1ea192ca98c0d
Sebastian <sebastian.rubio@uisek.edu.ec>
2026-05-26 09:24:59 -0500

    Finalizacion de prueba parcial

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,13 +1,11 @@
-from random import choices
-
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 class Trainer(models.Model):
     first_name = models.CharField(max_length=50, null=False)
     last_name = models.CharField(max_length=50, null=False)
     birth_date = models.DateField(null=False)
     level = models.IntegerField(null=False)
-    
     
     REGION_CHOICES = [
         ('K', 'Kanto'),
@@ -27,7 +25,16 @@ class Trainer(models.Model):
         default='NA'
     )
     
+    # Campo configurado para guardar el archivo físico y aceptar png, jpg y jpeg
+    picture = models.ImageField(
+        upload_to='trainers/', 
+        null=True, 
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])]
+    )
+
     def __str__(self):
+        # Concatenación de los campos reales
         return f"{self.first_name} {self.last_name}"
 
 
@@ -49,6 +56,17 @@ class Pokemon(models.Model):
     type = models.CharField(max_length=30, choices=POKEMON_TYPES, null=False)
     height = models.FloatField(null=False)
     weight = models.FloatField(null=False)
-    trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, null=True)
+    
+    # Única declaración de la llave foránea
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE, null=True, blank=True)
+    
+    # Campo configurado para guardar el archivo físico y aceptar png, jpg y jpeg
+    picture = models.ImageField(
+        upload_to='pokemons/', 
+        null=True, 
+        blank=True,
+        validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])]
+    )
+
     def __str__(self):
         return self.name
