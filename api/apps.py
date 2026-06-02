260b23c25e6fd59cbd8275278f84364ef570a823
laboratorio.6to piso <sebastian.rubio@uisek.edu.ec>
2026-06-02 11:02:52 -0500

    Intento de arreglar el caos

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
-class PokedexConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pokedex'
+    name = 'api'
